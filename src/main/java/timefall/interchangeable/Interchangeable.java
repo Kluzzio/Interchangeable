e1489d8971b88e4a57fe20e69520f388e7b50522
Kluzzio <kluzzio.amp@gmail.com>
2022-09-19 15:19:11 -0700

    Initial commit

---
+++ src/main/java/timefall/interchangeable/Interchangeable.java
@@ -0,0 +1,10 @@
+package timefall.interchangeable;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Interchangeable implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
