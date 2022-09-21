26b0193934755efda5dadb5965a9b61731b25b3a
Kluzzio <kluzzio.amp@gmail.com>
2022-09-21 15:00:37 -0700

    Clean up :)

--- src/main/java/timefall/interchangeable/mixins/ShapedRecipeMixin.java
+++ src/main/java/timefall/interchangeable/mixins/ShapedRecipeMixin.java
@@ -3,10 +3,8 @@ package timefall.interchangeable.mixins;
 import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.ShapedRecipe;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,11 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import timefall.interchangeable.config.ConfigManager;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Stream;
+import java.util.*;
 
 @Mixin(ShapedRecipe.class)
 public class ShapedRecipeMixin {
@@ -28,62 +22,44 @@ public class ShapedRecipeMixin {
     private static Ingredient recipeIngredient;
 
     @Inject(method = "matchesPattern", at = @At(value = "INVOKE", target = "Lnet/minecraft/recipe/Ingredient;test(Lnet/minecraft/item/ItemStack;)Z"), locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void getMeShit(CraftingInventory inv, int offsetX, int offsetY, boolean flipped, CallbackInfoReturnable<Boolean> cir,
+    private void getRecipeIngredient(CraftingInventory inv, int offsetX, int offsetY, boolean flipped, CallbackInfoReturnable<Boolean> cir,
                            int i, int j, int k, int l, Ingredient ingredient) {
         recipeIngredient = ingredient;
     }
 
     @ModifyArg(method = "matchesPattern", at = @At(value = "INVOKE", target = "Lnet/minecraft/recipe/Ingredient;test(Lnet/minecraft/item/ItemStack;)Z"))
-    private ItemStack fuckShitUp(ItemStack inputItemStack) {
+    private ItemStack interchangeShapedIngredients(ItemStack inputItemStack) {
         if (ConfigManager.SUCCESSFULLY_LOADED_CONFIG) {
             if ((Object) this instanceof ShapedRecipe shapedRecipe) {
-                // ID is the name of the output item for some reason?
-                Identifier identifier = shapedRecipe.getId();
-
-                // We need to look at the current slot
-                // See what ingredient should go there
                 Collection<Item> itemCollection = new ArrayList<>(List.of());
                 Arrays.stream(recipeIngredient.getMatchingStacks()).toList().forEach(itemStack -> itemCollection.add(itemStack.getItem()));
-                //if interchangeable
-                //look in config for the item to be interchangeable
-                for (String[] array : ConfigManager.CONFIG_FILE.getEquivalenceClasses()) {
-                    // Check that the normal ingredient is interchangeable
-                    for (Item validIngredientItem : itemCollection) {
-                        if (Arrays.stream(array).toList().contains(Registry.ITEM.getId(validIngredientItem).toString())) {
-                            // Check that the given ingredient is interchangeable with normal ingredient
-                            if (Arrays.stream(array).toList().contains(Registry.ITEM.getId(inputItemStack.getItem()).toString())) {
-                                // return normal ingredient
+
+                for (Item validIngredientItem : itemCollection) {
+                    for (String[] array : ConfigManager.CONFIG_FILE.getEquivalenceClasses()) {
+                        if (doesArrayContainItem(array, validIngredientItem))
+                            if (doesArrayContainItem(array, inputItemStack.getItem()))
                                 return new ItemStack(validIngredientItem, inputItemStack.getCount());
-                            }
-                        }
                     }
-                }
-                for (String key : ConfigManager.CONFIG_FILE.getSubstitutions().keySet()) {
-                    // Check that the normal ingredient is interchangeable
-                    for (Item validIngredientItem : itemCollection) {
-                        if (key.equals(Registry.ITEM.getId(validIngredientItem).toString())) {
-                            if (Arrays.stream(ConfigManager.CONFIG_FILE.getSubstitutions().get(key)).toList().contains(Registry.ITEM.getId(inputItemStack.getItem()).toString())) {
+                    Map<String, String[]> substitutions = ConfigManager.CONFIG_FILE.getSubstitutions();
+                    String itemKey = Registry.ITEM.getId(validIngredientItem).toString();
+                    if (substitutions.containsKey(itemKey)) {
+                        if (doesArrayContainItem(substitutions.get(itemKey), inputItemStack.getItem()))
+                            return new ItemStack(validIngredientItem, inputItemStack.getCount());
+                    }
+                    Map<String, String[]> recipeSpecific = ConfigManager.CONFIG_FILE.getRecipeSpecificEquivalenceClasses();
+                    String recipeKey = shapedRecipe.getId().toString();
+                    if (recipeSpecific.containsKey(recipeKey)) {
+                        if (doesArrayContainItem(recipeSpecific.get(recipeKey), validIngredientItem))
+                            if (doesArrayContainItem(recipeSpecific.get(recipeKey), inputItemStack.getItem()))
                                 return new ItemStack(validIngredientItem, inputItemStack.getCount());
-                            }
-                        }
-                    }
-                }
-                for (String key : ConfigManager.CONFIG_FILE.getRecipeSpecificEquivalenceClasses().keySet()) {
-                    // Check that the normal ingredient is interchangeable
-                    for (Item validIngredientItem : itemCollection) {
-                        if (key.equals(shapedRecipe.getId().toString())) {
-                            if (Arrays.stream(ConfigManager.CONFIG_FILE.getRecipeSpecificEquivalenceClasses().get(key)).toList().contains(Registry.ITEM.getId(validIngredientItem).toString())) {
-                                // Check that the given ingredient is interchangeable with normal ingredient
-                                if (Arrays.stream(ConfigManager.CONFIG_FILE.getRecipeSpecificEquivalenceClasses().get(key)).toList().contains(Registry.ITEM.getId(inputItemStack.getItem()).toString())) {
-                                    // return normal ingredient
-                                    return new ItemStack(validIngredientItem, inputItemStack.getCount());
-                                }
-                            }
-                        }
                     }
                 }
             }
         }
         return inputItemStack;
+    }
+
+    private static boolean doesArrayContainItem(String[] array, Item item) {
+        return Arrays.stream(array).toList().contains(Registry.ITEM.getId(item).toString());
     }
 }
--- src/main/java/timefall/interchangeable/mixins/ShapelessRecipeMixin.java
+++ src/main/java/timefall/interchangeable/mixins/ShapelessRecipeMixin.java
@@ -3,10 +3,7 @@ package timefall.interchangeable.mixins;
 import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.recipe.Ingredient;
-import net.minecraft.recipe.RecipeMatcher;
 import net.minecraft.recipe.ShapelessRecipe;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,10 +11,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import timefall.interchangeable.config.ConfigManager;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @Mixin(ShapelessRecipe.class)
 public class ShapelessRecipeMixin {
@@ -26,15 +24,8 @@ public class ShapelessRecipeMixin {
 
     @ModifyArg(method = "matches(Lnet/minecraft/inventory/CraftingInventory;Lnet/minecraft/world/World;)Z", at = @At(value = "INVOKE",
             target = "Lnet/minecraft/recipe/RecipeMatcher;addInput(Lnet/minecraft/item/ItemStack;I)V"))
-    private ItemStack breakMoreShit(ItemStack inputItemStack) {
+    private ItemStack interchangeShapelessIngredients(ItemStack inputItemStack) {
         if ((Object) this instanceof ShapelessRecipe shapelessRecipe) {
-            Collection<Item> itemCollection = new ArrayList<>(List.of());
-            shapelessRecipe.getIngredients().forEach(ingredient ->
-                    Arrays.stream(ingredient.getMatchingStacks()).toList().forEach(itemStack ->
-                            itemCollection.add(itemStack.getItem())
-                    )
-            );
-
             HashMap<Item, Integer> INGREDIENT_ITEMS = new HashMap<>();
             shapelessRecipe.getIngredients().forEach(ingredient ->
                     Arrays.stream(ingredient.getMatchingStacks()).toList().forEach(itemStack ->
@@ -43,51 +34,27 @@ public class ShapelessRecipeMixin {
                     )
             );
 
-            // Check that the normal ingredient is interchangeable
-            for (String[] array : ConfigManager.CONFIG_FILE.getEquivalenceClasses()) {
-                // Check that the normal ingredient is interchangeable
-                for (Item item : itemCollection) {
-                    if (Arrays.stream(array).toList().contains(Registry.ITEM.getId(item).toString())) {
-                        // Check that the given ingredient is interchangeable with normal ingredient
-                        if (Arrays.stream(array).toList().contains(Registry.ITEM.getId(inputItemStack.getItem()).toString())) {
-                            // Check that the given ingredient is interchangeable with normal ingredient
-                            // return normal ingredient
-                            if (SHAPELESS_INGREDIENTS.getOrDefault(item, 0) < INGREDIENT_ITEMS.getOrDefault(item, 0)) {
-                                SHAPELESS_INGREDIENTS.put(item, SHAPELESS_INGREDIENTS.getOrDefault(item, 0) + 1);
-                                return new ItemStack(item, inputItemStack.getCount());
-                            }
-                        }
-                    }
+            for (Item validIngredientItem : INGREDIENT_ITEMS.keySet()) {
+                for (String[] array : ConfigManager.CONFIG_FILE.getEquivalenceClasses()) {
+                    if (doesArrayContainItem(array, validIngredientItem))
+                        if (doesArrayContainItem(array, inputItemStack.getItem()))
+                            if (checkIngredientNotOverused(validIngredientItem, INGREDIENT_ITEMS))
+                                return makeUseOfIngredientAndDocument(validIngredientItem, inputItemStack);
                 }
-            }
-            for (String key : ConfigManager.CONFIG_FILE.getSubstitutions().keySet()) {
-                // Check that the normal ingredient is interchangeable
-                for (Item validIngredientItem : itemCollection) {
-                    if (key.equals(Registry.ITEM.getId(validIngredientItem).toString())) {
-                        if (Arrays.stream(ConfigManager.CONFIG_FILE.getSubstitutions().get(key)).toList().contains(Registry.ITEM.getId(inputItemStack.getItem()).toString())) {
-                            if (SHAPELESS_INGREDIENTS.getOrDefault(validIngredientItem, 0) < INGREDIENT_ITEMS.getOrDefault(validIngredientItem, 0)) {
-                                SHAPELESS_INGREDIENTS.put(validIngredientItem, SHAPELESS_INGREDIENTS.getOrDefault(validIngredientItem, 0) + 1);
-                                return new ItemStack(validIngredientItem, inputItemStack.getCount());
-                            }
-                        }
-                    }
+                Map<String, String[]> substitutions = ConfigManager.CONFIG_FILE.getSubstitutions();
+                String itemKey = Registry.ITEM.getId(validIngredientItem).toString();
+                if (substitutions.containsKey(itemKey)) {
+                    if (doesArrayContainItem(substitutions.get(itemKey), inputItemStack.getItem()))
+                        if (checkIngredientNotOverused(validIngredientItem, INGREDIENT_ITEMS))
+                            return makeUseOfIngredientAndDocument(validIngredientItem, inputItemStack);
                 }
-            }
-            for (String key : ConfigManager.CONFIG_FILE.getRecipeSpecificEquivalenceClasses().keySet()) {
-                // Check that the normal ingredient is interchangeable
-                for (Item validIngredientItem : itemCollection) {
-                    if (key.equals(shapelessRecipe.getId().toString())) {
-                        if (Arrays.stream(ConfigManager.CONFIG_FILE.getRecipeSpecificEquivalenceClasses().get(key)).toList().contains(Registry.ITEM.getId(validIngredientItem).toString())) {
-                            // Check that the given ingredient is interchangeable with normal ingredient
-                            if (Arrays.stream(ConfigManager.CONFIG_FILE.getRecipeSpecificEquivalenceClasses().get(key)).toList().contains(Registry.ITEM.getId(inputItemStack.getItem()).toString())) {
-                                // return normal ingredient
-                                if (SHAPELESS_INGREDIENTS.getOrDefault(validIngredientItem, 0) < INGREDIENT_ITEMS.getOrDefault(validIngredientItem, 0)) {
-                                    SHAPELESS_INGREDIENTS.put(validIngredientItem, SHAPELESS_INGREDIENTS.getOrDefault(validIngredientItem, 0) + 1);
-                                    return new ItemStack(validIngredientItem, inputItemStack.getCount());
-                                }
-                            }
-                        }
-                    }
+                Map<String, String[]> recipeSpecific = ConfigManager.CONFIG_FILE.getRecipeSpecificEquivalenceClasses();
+                String recipeKey = shapelessRecipe.getId().toString();
+                if (recipeSpecific.containsKey(recipeKey)) {
+                    if (doesArrayContainItem(recipeSpecific.get(recipeKey), validIngredientItem))
+                        if (doesArrayContainItem(recipeSpecific.get(recipeKey), inputItemStack.getItem()))
+                            if (checkIngredientNotOverused(validIngredientItem, INGREDIENT_ITEMS))
+                                return makeUseOfIngredientAndDocument(validIngredientItem, inputItemStack);
                 }
             }
         }
@@ -95,7 +62,20 @@ public class ShapelessRecipeMixin {
     }
 
     @Inject(method = "matches(Lnet/minecraft/inventory/CraftingInventory;Lnet/minecraft/world/World;)Z", at = @At("RETURN"))
-    private void fuckYouYes(CraftingInventory craftingInventory, World world, CallbackInfoReturnable<Boolean> cir) {
+    private void clearShapelessIngredientsHash(CraftingInventory craftingInventory, World world, CallbackInfoReturnable<Boolean> cir) {
         SHAPELESS_INGREDIENTS = new HashMap<>();
+    }
+
+    private static boolean doesArrayContainItem(String[] array, Item item) {
+        return Arrays.stream(array).toList().contains(Registry.ITEM.getId(item).toString());
+    }
+
+    private static boolean checkIngredientNotOverused(Item validIngredientItem, HashMap<Item, Integer> INGREDIENT_ITEMS) {
+        return SHAPELESS_INGREDIENTS.getOrDefault(validIngredientItem, 0) < INGREDIENT_ITEMS.getOrDefault(validIngredientItem, 0);
+    }
+
+    private static ItemStack makeUseOfIngredientAndDocument(Item validIngredientItem, ItemStack inputItemStack) {
+        SHAPELESS_INGREDIENTS.put(validIngredientItem, SHAPELESS_INGREDIENTS.getOrDefault(validIngredientItem, 0) + 1);
+        return new ItemStack(validIngredientItem, inputItemStack.getCount());
     }
 }
